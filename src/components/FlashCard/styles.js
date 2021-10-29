b9b22699fc72579085bfd89b1e173025f2df3e08
valdirpn <valdiraires.pn@gmail.com>
2021-10-29 02:40:58 -0300

    fix: card style

--- src/components/FlashCard/styles.js
+++ src/components/FlashCard/styles.js
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: transparent;
-  width: 300px;
-  height: 200px;
+  width: 100%;
+  max-width: 400px;
+  height: 300px;  
   perspective: 1000px;
 
   .flip-card-inner {
     position: relative;
     width: 100%;
     height: 100%;
-    text-align: center;
     transition: transform 0.8s;
     transform-style: preserve-3d;
   }
@@ -23,17 +23,26 @@ export const Container = styled.div`
     position: absolute;
     width: 100%;
     height: 100%;
+    text-align: center;
+    border-radius: 8px;
+
     -webkit-backface-visibility: hidden; /* Safari */
     backface-visibility: hidden;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 1rem;
   }
 
   .flip-card-front {
-    background-color: #bbb;
+    background-color: #fff;
     color: black;
   }
 
   .flip-card-back {
-    background-color: dodgerblue;
+    background-color: ${({ theme }) => theme.colors.primary};
     color: white;
     transform: rotateY(180deg);
   }
